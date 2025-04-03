958340c560893075f97f9912662ce24c9c8fb450
pubudiniashani <pubudiniashani2000@gmail.com>
2025-04-03 10:44:29 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import {Header} from "./components/Header.tsx";
+import {AboutMe} from "./components/Aboutme.tsx";
 
 function App() {
 
 
   return (
     <>
-
+      <Header></Header>
+       <AboutMe></AboutMe>
     </>
   )
 }
