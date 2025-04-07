09e3615289aa8dedbab54778811d2125d4457108
pubudiniashani <pubudiniashani2000@gmail.com>
2025-04-07 18:37:46 +0530

    projects updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,9 @@
 import image1 from "../assets/green shadow.png";
 import image2 from "../assets/pos.png";
 import image3 from "../assets/book store.png";
-import image4 from "../assets/chat app.png";
+import image4 from "../assets/mobile app.png";
 import image5 from "../assets/bookworm library.png";
-import image6 from "../assets/chat app.png";
+import image6 from "../assets/portfolio.png";
 
 
 export const Projects = () => {
@@ -11,39 +11,39 @@ export const Projects = () => {
         {
             title: "Green Shadow (Pvt) Ltd",
             description: "The crop monitoring system managing crops,fields,staff,vehicles developed using Java , Spring Boot , MySQL , JS and JWT authentication.",
-            image: image1, // Unique Image for Project 1
+            image: image1,
             link: "#",
             github: "https://github.com/pubudiniashani/green-shadow-react",
         },
         {
             title: "Point Of Sales System",
             description: "Developed a Point Of Sales system  using HTML , CSS and JS. The main functionalities include customer management, item management, and order processing.",
-            image: image2, // Unique Image for Project 2
+            image: image2,
             github: "https://github.com/pubudiniashani/Assignment-06",
         },
         {
             title: "Book Store Management System",
             description: "Developed a desktop application for managing a bookstore using Java and JavaFX. ",
-            image: image3, // Unique Image for Project 3
+            image: image3,
             github:"https://github.com/pubudiniashani/bookshop",
         },
         {
-            title: "Chat application",
-            description: "A modern portfolio showcasing projects and skills using React and Tailwind CSS.",
-            image: image4, // Unique Image for Project 3
-            github:"https://github.com/pubudiniashani/chat-application",
+            title: "Book review",
+            description: "The book review mobile app is designed by using react native and expo.",
+            image: image4,
+            github:"https://github.com/pubudiniashani/book-review",
         },
         {
             title: "Bookworm library",
             description: "Developed a desktop application using Java , MySQL and Hibernate ORM. The application  manages books, customers, and orders.",
-            image: image5, // Unique Image for Project 3
+            image: image5,
             github:"https://github.com/pubudiniashani/Bookworm",
         },
         {
-            title: "Chat application",
-            description: "A modern portfolio showcasing projects and skills using React and Tailwind CSS.",
-            image: image6, // Unique Image for Project 3
-            github:"",
+            title: "Porfolio",
+            description: "My portfolio is developed using React and Tailwind CSS.",
+            image: image6,
+
         },
     ];
 
@@ -59,7 +59,7 @@ export const Projects = () => {
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 ">
                     {projects.map((project, index) => (
                         <div key={index}
-                             className="bg-white rounded-xl shadow-lg overflow-hidden transform hover:scale-105 transition duration-300">
+                             className="bg-[#c5cae9] rounded-xl shadow-lg overflow-hidden transform hover:scale-105 transition duration-300">
                             <img src={project.image} alt={project.title} className="w-full h-48 object-cover"/>
                             <div className="p-6">
                                 <h3 className="text-xl font-semibold text-gray-800">{project.title}</h3>
